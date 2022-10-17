237840eec15f9792f0c3f75facb72430683de329
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2022-10-17 13:24:52 +0200

    Add gitignore

--- Project01/Project01.ino
+++ Project01/Project01.ino
@@ -19,7 +19,7 @@
 #define INITIAL_STATE 100
 #define IN_GAME 101
 
-#define MAX_PENALTIES 3 
+#define MAX_PENALTIES 3
 
 #include <avr/sleep.h>
 #include "Timer.h"
@@ -34,8 +34,8 @@ int incDiff;
 
 unsigned int score;
 
-int leds[4] = {LED_PIN1, LED_PIN2, LED_PIN3, LED_PIN4};
-int buttons[4] = {BUTTON_PIN1, BUTTON_PIN2, BUTTON_PIN3, BUTTON_PIN4};
+int leds[4] = { LED_PIN1, LED_PIN2, LED_PIN3, LED_PIN4 };
+int buttons[4] = { BUTTON_PIN1, BUTTON_PIN2, BUTTON_PIN3, BUTTON_PIN4 };
 
 Timer timer(MILLIS);
 int patternLeds[4];
@@ -61,8 +61,8 @@ void setup() {
   PCICR |= B00000100;
   // Select PCINT20 Bit4 = 1 (Pin D4)
   // Select PCINT21 Bit5 = 1 (Pin D5)
-  PCMSK2 |= bit (PCINT20);
-  PCMSK2 |= bit (PCINT21);
+  PCMSK2 |= bit(PCINT20);
+  PCMSK2 |= bit(PCINT21);
   Serial.println("\nWelcome to the Catch the Led Pattern Game. Press Key T1 to Start\n");
   timer.start();
 }
@@ -70,52 +70,51 @@ void setup() {
 /** AttachInterrupt funziona solo con i pin 2 e 3, per utilizzare altri pin con gli interrupt 
     utilizzo la funzione ISR */
 ISR(PCINT2_vect) {
-    sleep_disable();
-}  
+  sleep_disable();
+}
 
 void loop() {
-  delay(5);   //delay per fading
+  delay(5);  //delay per fading
 
   switch (state) {
-    case INITIAL_STATE :
+    case INITIAL_STATE:
       if (isButtonPressed(START_GAME_BUTTON)) {
-      difficulty = (analogRead(POTENZIOMETRO) / 256) + 1;
-      
-      Serial.println("GO!\n");
-      Serial.print("Difficulty: ");
-      Serial.println(difficulty);
-      
-      digitalWrite(LED_PIN_ROSSO, LOW); 
-      state = IN_GAME;
-      startGame(difficulty);
-    } else {
-      currIntensity += fadeAmount;
-      if (currIntensity <= 0 || currIntensity >= 255) {
-        fadeAmount = -fadeAmount;
-      }
-      analogWrite(LED_PIN_ROSSO, currIntensity); 
-    }
-
-    if(timer.read() >= 10000) {
-      timer.stop();
-      Serial.println("Sleep mode: On");
-      digitalWrite(LED_PIN_ROSSO, LOW); 
-      sleep();
-      Serial.println("Sleep mode: Off");
-      delay(300);
-      timer.start();
-    }
-    break;
-    case IN_GAME :
-      digitalWrite(LED_PIN_ROSSO, LOW); 
-      if(penalty >= MAX_PENALTIES) {
-        Serial.println("\nGAME OVER!");
-        Serial.print("Final Score: ");
-        Serial.println(score);
-
+        difficulty = (analogRead(POTENZIOMETRO) / 256) + 1;
         score = 0;
         incDiff = 0;
         penalty = 0;
+        Serial.println("GO!\n");
+        Serial.print("Difficulty: ");
+        Serial.println(difficulty);
+
+        digitalWrite(LED_PIN_ROSSO, LOW);
+        state = IN_GAME;
+        startGame(difficulty);
+      } else {
+        currIntensity += fadeAmount;
+        if (currIntensity <= 0 || currIntensity >= 255) {
+          fadeAmount = -fadeAmount;
+        }
+        analogWrite(LED_PIN_ROSSO, currIntensity);
+      }
+
+      if (timer.read() >= 10000) {
+        timer.stop();
+        Serial.println("Sleep mode: On");
+        digitalWrite(LED_PIN_ROSSO, LOW);
+        sleep();
+        Serial.println("Sleep mode: Off");
+        delay(300);
+        timer.start();
+      }
+      break;
+      
+    case IN_GAME:
+      digitalWrite(LED_PIN_ROSSO, LOW);
+      if (penalty >= MAX_PENALTIES) {
+        Serial.println("\nGAME OVER!");
+        Serial.print("Final Score: ");
+        Serial.println(score);
 
         state = INITIAL_STATE;
         delay(10000);
@@ -124,12 +123,12 @@ void loop() {
       } else {
         startGame(difficulty);
       }
-    break;
+      break;
   }
 }
 
-void initialize(){
-  for(int i = 0; i < GAME_LEDS; i++) {
+void initialize() {
+  for (int i = 0; i < GAME_LEDS; i++) {
     pinMode(leds[i], OUTPUT);
     pinMode(buttons[i], INPUT);
     patternLeds[i] = INCORRECT;
@@ -138,7 +137,7 @@ void initialize(){
   pinMode(LED_PIN_ROSSO, OUTPUT);
 }
 
-void sleep(){
+void sleep() {
   Serial.flush();
   delay(100);
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
@@ -146,8 +145,8 @@ void sleep(){
   sleep_mode();
 }
 
-void startGame(int difficulty) {  
-  int initialWaitingTime = random(1,6) * 1000;
+void startGame(int difficulty) {
+  int initialWaitingTime = random(1, 6) * 1000;
   int patternTime = (10 - difficulty - incDiff) * 1000;
   int availableTime = patternTime;
   int turnLost = 0;
@@ -157,16 +156,16 @@ void startGame(int difficulty) {
   setLedsState(LOW);
 
   Serial.print("\nSeconds to recreate the pattern: ");
-  Serial.print(availableTime/1000);
-  
+  Serial.print(availableTime / 1000);
+
   delay(initialWaitingTime);
-  
+
   ledsOn = createPattern();
   timer.start();
 
-  while(timer.read() <= patternTime && !turnLost) {
-    for(int i = 0; i < GAME_LEDS; i++){
-      if(isButtonPressed(i)){
+  while (timer.read() <= patternTime && !turnLost) {
+    for (int i = 0; i < GAME_LEDS; i++) {
+      if (isButtonPressed(i)) {
         addPenalty("\nPENALTY: TOO EARLY");
         turnLost = 1;
         break;
@@ -179,41 +178,41 @@ void startGame(int difficulty) {
   setLedsState(LOW);
   timer.start();
 
-  while(timer.read() <= availableTime && !turnLost) {
-    if(ledsOn == correctLeds) {
+  while (timer.read() <= availableTime && !turnLost) {
+    if (ledsOn == correctLeds) {
       score++;
       timer.pause();
       Serial.print("\nNew point! Score: ");
       Serial.println(score);
-      
-      switch(difficulty) {
-          case 1:
-            incDiff == 8 ? incDiff=incDiff : incDiff++;
-            break;
-          case 2:
-            incDiff == 7 ? incDiff=incDiff : incDiff++;
-            break;
-          case 3:
-            incDiff == 6 ? incDiff=incDiff : incDiff++;
-            break;
-          case 4:
-            incDiff == 5 ? incDiff=incDiff : incDiff++;
-            break;
+
+      switch (difficulty) {
+        case 1:
+          incDiff == 8 ? incDiff = incDiff : incDiff++;
+          break;
+        case 2:
+          incDiff == 7 ? incDiff = incDiff : incDiff++;
+          break;
+        case 3:
+          incDiff == 6 ? incDiff = incDiff : incDiff++;
+          break;
+        case 4:
+          incDiff == 5 ? incDiff = incDiff : incDiff++;
+          break;
       }
       break;
-    }    
-    
-    for(int i = 0; i < GAME_LEDS; i++) {
-      if(isButtonPressed(i)) {
-          if(patternLeds[i] == CORRECT) {
-            digitalWrite(leds[i], HIGH);
-            patternLeds[i] = TAKEN;
-            correctLeds++;
+    }
+
+    for (int i = 0; i < GAME_LEDS; i++) {
+      if (isButtonPressed(i)) {
+        if (patternLeds[i] == CORRECT) {
+          digitalWrite(leds[i], HIGH);
+          patternLeds[i] = TAKEN;
+          correctLeds++;
         } else if (patternLeds[i] == INCORRECT) {
-            addPenalty("\nPENALTY: WRONG PATTERN");
-            timer.pause();
-            turnLost = 1;
-            break;
+          addPenalty("\nPENALTY: WRONG PATTERN");
+          timer.pause();
+          turnLost = 1;
+          break;
         }
       }
     }
@@ -221,15 +220,15 @@ void startGame(int difficulty) {
 
   delay(1000);
   setLedsState(LOW);
-  
-  if(timer.read() >= availableTime) {
+
+  if (timer.read() >= availableTime) {
     addPenalty("\nPENALTY: TIME OVER");
   }
   return;
 }
 
 void setLedsState(int state) {
-  for(int i = 0; i < GAME_LEDS; i++) {
+  for (int i = 0; i < GAME_LEDS; i++) {
     digitalWrite(leds[i], state);
   }
 }
@@ -238,7 +237,7 @@ int createPattern() {
   int ledState = 0;
   int ledsOn = 0;
 
-  for(int i = 0; i < GAME_LEDS; i++) {
+  for (int i = 0; i < GAME_LEDS; i++) {
     ledState = random(0, 2);
     digitalWrite(leds[i], ledState);
     patternLeds[i] = ledState;
@@ -257,8 +256,8 @@ void addPenalty(String msg) {
   delay(1000);
 }
 
-bool isButtonPressed(int button){
-  if(digitalRead(button+2) == HIGH){
+bool isButtonPressed(int button) {
+  if (digitalRead(button + 2) == HIGH) {
     return true;
   }
   return false;
